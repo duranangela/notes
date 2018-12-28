08da7a961c5f3d0298735c82e9e578312a4f38ba
Angela Duran <duranangela19@gmail.com>
2018-12-28 10:07:46 -0700

    Change todo to note

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const newTodo = () => {
+const newNote = () => {
   let tag = $('#tag').val();
   let note = $('#note').val();
 
@@ -6,26 +6,26 @@ const newTodo = () => {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({"note": `${note}`, "tag": `${tag}`})
-  }).then(response => getTodos())
-  .then(todos => $('#note').val(''));
+  }).then(response => getNotes())
+  .then(notes => $('#note').val(''));
 }
 
-const getTodos = () => {
-  $(".todo-list").empty();
+const getNotes = () => {
+  $(".note-list").empty();
   fetch("https://ba2w3lgnm5.execute-api.us-west-1.amazonaws.com/prod/todos")
     .then(response => response.json())
-    .then(todos => listTodos(todos))
+    .then(notes => listNotes(notes))
     .catch(error => console.error({error}));
 };
 
-const listTodos = (todos) => {
-  todos = todos["Items"].sort((a,b) => (b.created_at) - (a.created_at));
-  todos.forEach(todo => {
-    $('.todo-list').append(`
-      <div class='todos'>
-        <div>${todo.note}</div>
-        <div>${todo.tag}</div>
-        <div>${new Date(todo.created_at * 1000).toLocaleDateString()} ${new Date(todo.created_at * 1000).toLocaleTimeString()}</div>
+const listNotes = (notes) => {
+  notes = notes["Items"].sort((a,b) => (b.created_at) - (a.created_at));
+  notes.forEach(note => {
+    $('.note-list').append(`
+      <div class='notes'>
+        <div>${note.note}</div>
+        <div>${note.tag}</div>
+        <div>${new Date(note.created_at * 1000).toLocaleDateString()} ${new Date(note.created_at * 1000).toLocaleTimeString()}</div>
       </div>
       <br>
       `);
@@ -34,4 +34,4 @@ const listTodos = (todos) => {
 
 
 
-getTodos()
+getNotes()
